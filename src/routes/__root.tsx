b2aeba30f6b0bb77d21f8791d01cc9080d84afa6
WorkersComma <workerscomma@gmail.com>
2025-08-18 15:29:19 +0900

    feat: React Query 적용

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,8 @@
 import { Outlet, createRootRoute, useLocation } from "@tanstack/react-router";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { Header } from "../shared/components/Header";
+import { queryClient } from "../shared/queryClient";
 
 export const Route = createRootRoute({
   component: () => {
@@ -12,7 +14,7 @@ export const Route = createRootRoute({
         : "bg-[#EDF4FF]";
 
     return (
-      <>
+      <QueryClientProvider client={queryClient}>
         <Header />
         <section
           className={"min-h-[calc(100vh-var(--header-height))] " + bgColor}
@@ -20,7 +22,7 @@ export const Route = createRootRoute({
           <Outlet />
         </section>
         <TanStackRouterDevtools />
-      </>
+      </QueryClientProvider>
     );
   },
 });
